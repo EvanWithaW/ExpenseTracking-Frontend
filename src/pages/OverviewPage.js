8cf9ee2c9cc9403aeb11585b99271ca6e1cace4f
EvanWithaW <evanweidner5@gmail.com>
2024-08-04 21:39:32 -0500

    add fetching user info to enable personalizing overview

--- src/pages/OverviewPage.js
+++ src/pages/OverviewPage.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function OverviewPage() {
-    // console.log('Rendering OverviewPage');
+    const [fullName, setFullName] = useState('');
+
+    useEffect(() => {
+        const getFullName = async () => {
+            const tokenStorage = sessionStorage.getItem('token');
+            if (tokenStorage) {
+                const token = JSON.parse(tokenStorage).token;
+                try {
+                    const response = await fetch('http://localhost:8080/user/info', {
+                        method: 'GET',
+                        headers: {
+                            "Authorization": `Bearer ${token}`,
+                            "Content-Type": "application/json"
+                        }
+                    });
+                    if (response.ok) {
+                        try {
+                            const data = await response.json();
+                            // console.log(await response);
+                            setFullName(data.name);
+                        } catch (jsonError) {
+                            console.error('Error parsing JSON:', jsonError);
+                        }
+                    } else {
+                        console.error('Failed to fetch fullname');
+                    }
+                } catch (error) {
+                    console.error('Error fetching fullname:', error);
+                }
+            }
+        };
+
+        getFullName();
+    }, []);
+
     return (
         <div>
             <h1>Expenses Overview</h1>
-            <p>This is the overview page</p>
+            <p>This is the overview page for {fullName}</p>
         </div>
     );
 }
