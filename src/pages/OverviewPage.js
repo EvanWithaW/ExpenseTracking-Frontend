22a8d6b9464038427126a64712de0265eca645ac
EvanWithaW <evanweidner5@gmail.com>
2024-08-12 00:44:46 -0500

    Add functionality to list expenses on overview page, add space for future overall metrics area

--- src/pages/OverviewPage.js
+++ src/pages/OverviewPage.js
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
+import '../styles/OverviewPage.css';
 
 function OverviewPage() {
     const [fullName, setFullName] = useState('');
+    const [expenses, setExpenses] = useState([]);
 
     useEffect(() => {
         const getFullName = async () => {
@@ -19,7 +21,6 @@ function OverviewPage() {
                     if (response.ok) {
                         try {
                             const data = await response.json();
-                            // console.log(await response);
                             setFullName(data.name);
                         } catch (jsonError) {
                             console.error('Error parsing JSON:', jsonError);
@@ -33,13 +34,59 @@ function OverviewPage() {
             }
         };
 
+        const getExpenses = async () => {
+            const tokenStorage = sessionStorage.getItem('token');
+            if (tokenStorage) {
+                const token = JSON.parse(tokenStorage).token;
+                try {
+                    const response = await fetch('http://localhost:8080/getExpenses', {
+                        method: 'GET',
+                        headers: {
+                            "Authorization": `Bearer ${token}`,
+                            "Content-Type": "application/json"
+                        }
+                    });
+                    if (response.ok) {
+                        try {
+                            const data = await response.json();
+                            setExpenses(data);
+                        } catch (jsonError) {
+                            console.error('Error parsing JSON:', jsonError);
+                        }
+                    } else {
+                        console.error('Failed to fetch expenses');
+                    }
+                } catch (error) {
+                    console.error('Error fetching expenses:', error);
+                }
+            }
+        };
+
         getFullName();
+        getExpenses();
     }, []);
 
     return (
         <div>
             <h1>Expenses Overview</h1>
             <p>This is the overview page for {fullName}</p>
+            <div className="overviewPageInfo">
+                <div className="expenseList">
+                    <h3> Expenses </h3>
+                    <ul>
+                        {Array.isArray(expenses) && expenses.length > 0 ? (
+                            expenses.map((expense, index) => (
+                                <li key={index}>{expense.description}: ${expense.amount} {expense.addedDate} </li>
+                            ))
+                        ) : (
+                            <li>No Expenses Logged</li>
+                        )}
+                    </ul>
+                </div>
+                <div className="overallMetrics">
+                    <h3>Overall Metrics</h3>
+                </div>
+            </div>
         </div>
     );
 }
